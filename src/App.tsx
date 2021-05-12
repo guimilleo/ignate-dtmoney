4ad7b9da3e8068987fd2ba8e2172ec10a97740a0
Guilherme Milléo <guilherme@e-peca.com.br>
2021-05-12 17:52:35 -0300

    aula Criando estilos gloais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-// import './App.css'
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257E6;
-`
+import {GlobalStyle} from './styles/global'
 
 // esse export funciona melhor para imports da IDE
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
+      <GlobalStyle/>
     </div>
   );
 }
